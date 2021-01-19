173f75e49ac2aaf8c5042f4b3415d8b826cf563c
muzikang123 <7290960@qq.com>
2021-01-19 20:19:56 +0800

    第一次提交day01.py

---
+++ day01.py
@@ -0,0 +1,47 @@
+# name1 = '苹果'
+# price1 = 20
+# number1 = 25
+# quality1 = 'A+'
+#
+# name2 = '橘子'
+# price2 = 10
+# number2 = 15
+# quality2 = 'A+'
+#
+# name3 = '香蕉'
+# price3 = 6
+# number3 = 10
+# quality3 = 'A+'
+# print("-" * 40, '水果商城商城' + '-' * 40)
+# print("名称：" + name1, "价格:" + str(price1), "数量:" + str(number1), "品质:" + quality1)
+# print('-' * 50)
+# print("名称：" + name2, "价格:" + str(price2), "数量:" + str(number2), "品质:" + quality2)
+# print('-' * 50)
+# print("名称：" + name3, "价格:" + str(price3), "数量:" + str(number3), "品质:" + quality3)
+# print('-' * 50)
+# print("总价格:", +price1 * number1 + price2 * number2 + price3 * number3)
+# print('-' * 50)
+
+# print('-'*20+"衣服商城"+'-'*20)
+# brand1 = '耐克'
+# name1 = '羽绒服'
+# price1 = 500
+# quality1 = 'A+'
+# number1 = 1
+#
+# brand2 = '安踏'
+# name2 = '牛仔裤'
+# price2 = 300
+# quality2 = 'A+'
+# number2 = 2
+#
+# brand3 = '阿迪'
+# name3 = "毛衣"
+# price3 = 300
+# quality3 = 'A+'
+# number3 = 2
+# print("品牌","衣服名称","价格","品质","数量")
+# print(brand1, name1, price1, quality1, number1, price1*number1)
+# print(brand2, name2, price2, quality2, number2, price2*number2)
+# print(brand3, name3, price3, quality3, number3, price3*number3)
+# print("-"*50)
