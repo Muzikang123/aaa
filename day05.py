02630f98f4e0636df3903ab9efba8af704484e12
Muzikang123 <7290960@qq.com>
2021-02-18 13:39:13 +0800

    银行连接数据库代码

--- day05.py
+++ day05.py
@@ -1,4 +1,5 @@
 import random
+
 # 银行的名称
 bank_name = "北京市工商银行昌平支行"
 
@@ -11,7 +12,6 @@ users = {}
 }
 
 '''
-
 # 欢迎界面
 welcome = '''
 *********************************
@@ -55,56 +55,34 @@ def bank_addUser(account,username,password,money,country,province,street,door):
             "door":door,
             "bank_name":bank_name
         }
+
         return 1
-#存钱的逻辑
-def bank_saveMoney(account,saveMoney):
-    values = users.values()
-    for i in values:
-        if account in i.values():
-            i["money"]=i["money"]+saveMoney
-            return True
-        else:
-            return False
+
 #查询功能的逻辑
-def bank_queryMoney( username, password,money,country,province,street,door):
-    values = users.values()
-    if username in users:
-        for i in values:
-            if password in i.values():
-                users[username]={
-                    "money":money,
-                    "password":password,
-                    "country":country,
-                    "province":province,
-                    "street":street,
-                     "door":door,
-                    "bank_name": bank_name
-                }
-                return 1
+def bank_queryMoney(username,password):
+    import pymysql
+    con = pymysql.connect(host="localhost",user="root",password="",database="bank")
+    cursor = con.cursor()
+    sql = "select * from user where 姓名 = %s"
+    param=[username]
+    num=cursor.execute(sql,param)
+    data = cursor.fetchall()
+    if num==0:
+        print("没有此用户")
+    elif password != data[0][2]:
+        print("密码输入错误")
     else:
-        return 2
-
-
+        print("查询信息的账号为：",data[0][0],",姓名",data[0][1],",密码：",data[0][2],",余额：",data[0][3],
+              ",国家",data[0][4],",省份",data[0][5],",街道",data[0][6],",门牌号",data[0][7])
+        cursor.fetchall()
+        con.commit()
+        cursor.close()
+        con.close()
 def queryMoney():
     username=input("请输入用户名")
     password=input("请输入你的密码:")
-    money=users[username]["money"]
-    country=users[username]["country"]
-    province = users[username]["province"]
-    street = users[username]["street"]
-    door = users[username]["door"]
-    query=bank_queryMoney(username,password,money,country,province,street,door)
-    if query==1:
-        print("当前账号为：",username)
-        print("当前密码为",users[username]["password"])
-        print("当前余额为：",users[username]["money"])
-        print("国家为：", users[username]["country"])
-        print("城市为：", users[username]["province"])
-        print("街道：", users[username]["street"])
-        print("门牌号：", users[username]["door"])
-        print("开户行名为:", users[username]["bank_name"])
-    elif query==2:
-        print("该用户不存在")
+    bank_queryMoney(username,password)
+
 
 # 普通的开户方法
 def addUser():
@@ -119,9 +97,9 @@ def addUser():
     street = input("请输入您所在的街道：")
     door = input("请输入您地址的门牌号：")
     account =  getRandom() # 获取随机账号
-
     # 将数据传给银行
     status = bank_addUser(account,username,password,money,country,province,street,door)
+    status1 = addSql(account,username,password,money,country,province,street,door)
     if status == 3:
         print("对不起，银行用户已满！请携带证件到其他银行办理！")
     elif status == 2:
@@ -138,9 +116,24 @@ def addUser():
         print("您所在街道为:", users[username]["street"])
         print("您所在门牌号为:", users[username]["door"])
         print("开户行名为:", users[username]["bank_name"])
+    elif status1==1:
+        print("保存成功")
+def addSql(account,username,password,money,country,province,street,door) :
+    import pymysql
+    con=pymysql.connect(host="localhost",user="root",password="",database="bank")#通过pymysql获取连接
+    cursor=con.cursor()
+    sql="insert into user values(%s,%s,%s,%s,%s,%s,%s,%s)"
+    param = [account,username,password,money,country,province,street,door]
+    print(param)
+    num= cursor.execute(sql,param)
+    print("影响了",num,"行数据")
+    con.commit()
+    cursor.close()
+    con.close()
 
+#存钱的逻辑
 def saveMoney():
-    account = input("请输入账号:")
+    username= input("请输入账户:")
     saveMoney = input("请输入金额:")
     while True:
         if saveMoney.isdigit():
@@ -148,78 +141,120 @@ def saveMoney():
              break
         else:
             print("余额输入错误，请重新输入！")
-    save=bank_saveMoney(account,saveMoney)
-    if save==True:
-        print("存钱成功")
-        print(users)
-    else:
-        print("此用户不存在")
+    saveMoneysql(username,saveMoney)
 
+def saveMoneysql(username,saveMoney):
+    import pymysql
+    con = pymysql.connect(host="localhost",user="root",password="",database="bank")
+    cursor = con.cursor()
+    sql = "select * from user where 姓名 = %s"
+    param=[username]
+    num=cursor.execute(sql,param)
+    if num==0:
+        print("没有此用户")
+    else:
+        data = cursor.fetchall()
+        newsaveMoney=saveMoney+int(data[0][3])
+        print("存钱成功，本次存入金额：",saveMoney,"账户余额：",newsaveMoney)
+        sql1="update user set 存款余额=%s where 姓名=%s"
+        param1=[newsaveMoney,username]
+        cursor.execute(sql1,param1)
+        cursor.fetchall()
+        con.commit()
+        cursor.close()
+        con.close()
 
 #转账核心方法
 def transfer(to_username, out_username, out_username_pass, out_money):
-    if out_username in users and to_username in users:  #判断库里是否转出账户和装入账户
-        if out_username_pass==users[out_username]["password"]:  #判断装出账户密码是否正确
-            if out_money<=users[out_username]["money"]:    #判断转出账户金额是否够
-                   return 0
-            else:
-                  return 3
-        else:
-            return 2
+    import pymysql
+    con = pymysql.connect(host="localhost",user="root",password="",database="bank")
+    cursor = con.cursor()
+    sql = "select * from user where 姓名 = %s"
+    param=[out_username]
+    num=cursor.execute(sql,param)
+    data = cursor.fetchall()
+    sql0 = "select * from user where 姓名 = %s"
+    param=[to_username]
+    num0=cursor.execute(sql0,param)
+    data1=cursor.fetchall()
+    if num0==0:
+        print("没有此用户")
+    elif num==0:
+        print("没有此用户")
+    elif out_username_pass != data[0][2]:
+        print("密码输入错误")
+    elif out_money>data[0][3]:
+        print("账户余额不足!!")
     else:
-        return 1
+        newMoney=data[0][3]-out_money
+        newMoney2=data1[0][3]+out_money
+        print("转钱成功，本次转出：",out_money,"账户余额：",newMoney)
+        sql2="update user set 存款余额=%s where 姓名=%s"
+        param2=[newMoney,out_username]
+        cursor.execute(sql2,param2)
+        sql3="update user set 存款余额=%s where 姓名=%s"
+        param3=[newMoney2,to_username]
+        cursor.execute(sql3,param3)
+        cursor.fetchall()
+        con.commit()
+        cursor.close()
+        con.close()
 
 #转账方法
 def transferAccounts():
-    out_username=input("请输入转出账户：")
-    to_username=input("请输入转入账户：")
-    out_username_pass = input("请输入转出账户的密码：")
-    out_money = int(input("请输入转账金额："))
+    out_username=input("转入账户：")
+    to_username=input("转出账户：")
+    out_username_pass = input("转出账户的密码：")
+    out_money = int(input("转账金额："))
+    transfer(out_username,to_username,out_username_pass,out_money)
 
-    status4=transfer(to_username,out_username,out_username_pass,out_money)
-    if status4==1:
-        print("转出账户或转入账户不存在！")
-    elif status4==2:
-        print("转出账户的密码不正确！！!")
-    elif status4==3:
-        print("金额不足！！！")
-    elif status4==0:
-        users[out_username]["money"]=users[out_username]["money"]-out_money
-        users[to_username]["money"]=users[to_username]["money"]+out_money
-        print("转账成功！","\n",out_username,"余额为：",users[out_username]["money"],"\n",to_username,"余额为：",users[to_username]["money"])
-
-def bank_withdraw(username,password,money):
-
-    for value in users.values():
-        if username not in value.values():
-            return 1
-        elif password not in value.values():
-            return 2
-        elif money > value["money"]:
-            return 3
-        else:
-            value["money"] = value["money"] - money
-            users[username]={
-                "username":username,
-                "password":password,
-                "money":value["money"]
-            }
-            return 0
-
+#取钱功能
 def withdraw():
     username = input("请输入姓名：")
     password = input("请输入取款密码：")
     money = int(input("请输入您要取的金额："))
-    returnvalue= bank_withdraw(username,password,money)
-    if returnvalue == 1:
-        print("用户不存在")
-    elif returnvalue == 2:
-        print("密码不正确")
-    elif returnvalue == 3:
-        print("账户余额不足")
-    elif returnvalue == 0:
-        print("以取出：",money,"元","余额还剩：",users[username]["money"])
-        print(users)
+    drawMoneysql(username,password,money)
+
+def drawMoneysql(username,password,money):
+    import pymysql
+    con = pymysql.connect(host="localhost",user="root",password="",database="bank")
+    cursor = con.cursor()
+    sql = "select * from user where 姓名 = %s"
+    param=[username]
+    num=cursor.execute(sql,param)
+    data = cursor.fetchall()
+    if num==0:
+        print("没有此用户")
+    elif password != data[0][2]:
+        print("密码输入错误")
+    elif money>data[0][3]:
+        print("账户余额不足!!")
+    else:
+        newMoney=int(data[0][3])-money
+        print("取钱成功，本次取出金额：",money,"账户余额：",newMoney)
+        sql2="update user set 存款余额=%s where 姓名=%s"
+        param2=[newMoney,username]
+        cursor.execute(sql2,param2)
+        cursor.fetchall()
+        con.commit()
+        cursor.close()
+        con.close()
+
+def writeFile():
+    f=open("bank.txt","w+",encoding="utf-8")
+    for key in users.keys():
+        string=""
+        string=string+key
+        string = string+ "," + users[key]["account"]
+        string = string + "," + users[key]["password"]
+        string = string + "," + str(users[key]["money"])
+        string = string + "," + users[key]["country"]
+        string = string + "," + users[key]["province"]
+        string = string+ ","  + users[key]["street"]
+        string = string + "," + users[key]["door"]
+        string = string + "," + users[key]["bank_name"]
+        f.write(string + "\n")
+    f.close()
 while True:
     print(welcome) # 打印欢迎信息
     chose = input("请输入您的业务编号：")
@@ -227,10 +262,13 @@ while True:
         addUser()
     elif chose == "2":
         saveMoney()
+        writeFile()
     elif chose == "3":
         withdraw()
+        writeFile()
     elif chose == "4":
         transferAccounts()
+        writeFile()
     elif chose == "5":
         queryMoney()
     elif chose  == "6":
@@ -238,5 +276,4 @@ while True:
         break
     else:
         print("输入非法！请重新输入！")
-
 
